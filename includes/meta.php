c98bffafce1c6de88f6cf8a7972914d03c46e5a8
WEB24H <contact@webmakeover24h.com>
2017-05-05 11:16:47 -0400

    Add files via upload

---
+++ includes/meta.php
@@ -0,0 +1,39 @@
+     <?php ;?>
+
+<!-- Meta Tags 
+        ================================ -->
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1">
+        <meta name="ROBOTS" content="INDEX, FOLLOW">
+        <meta name="keywords" content="uber site, emmanuel site, examples, etc.">
+        <meta name="description" content="Uber site example">
+        <meta name="author" content="Emmanuel Onyedikachi">
+        
+        
+        
+          <!-- Title Tag
+        ================================ -->
+        <title>Emmanuel Website</title>
+        
+        
+          <!-- Main Stylesheet Tag
+        ================================ -->
+          <link rel="stylesheet" href="css/main.css" />
+          
+          
+          <!-- Javascript Tag 
+          ============================== -->
+          <script  src="https://code.jquery.com/jquery-3.1.1.js"  integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>
+          <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+          
+          
+           <!-- Bootstrap Stylesheet
+        ================================ -->
+          <!-- Latest compiled and minified CSS -->
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+          <!-- Optional theme -->
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+          <!-- Latest compiled and minified JavaScript -->
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+          
