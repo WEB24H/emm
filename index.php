648fb268d7a61877caafe221a4b4d4be7d7a8cf2
WEB24H <contact@webmakeover24h.com>
2017-05-05 11:18:46 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+  <?php
+  include 'includes/header.php'
+  ?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+    </head>
+    
+    
+    
+    <body>
+        
+                  <!-- TEST MAIN.CSS
+        ================================ -->
+                  <button id="myButton">Send Form</button>
+        
+        
+                  <!-- TEST BOOTSTRAP
+        ================================ -->
+                     <button class="btn btn-success">Submit Form</button>
+        
+               
+          <!-- FOOTER.PHP
+        ================================ -->
+ <?php  include 'includes/footer.php'?>
+    </body>
+</html>
